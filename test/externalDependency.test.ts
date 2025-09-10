be06b216a85fc933a377e5175d5eb76cd1d08e60
Michele Palamidessi <michele.palamidessi@igenius.ai>
2025-09-10 10:33:01 +0200

    add: how to test a common js external dependency

--- test/externalDependency.test.ts
+++ test/externalDependency.test.ts
@@ -1,15 +1,30 @@
-import { beforeEach, test } from "node:test";
-import Pino from "pino";
+import { beforeEach, describe, test } from "node:test";
+import { equal } from "node:assert";
+import * as Pino from "pino";
+import * as PinoWrapped from "../src/pinoWrapper";
 import { stub, restore } from "sinon";
 
 beforeEach(() => {
   restore();
 });
 
-test.skip("Stub an external dependency", () => {
-  const stubbedPino = stub(Pino);
+describe("Stub an external common js dependency need a wrapper", () => {
+  test("When no wrapper is used an error is thrown", () => {
+    try {
+      stub(Pino, "pino");
+    } catch (e) {
+      equal(
+        (e as Error).message.includes(
+          "Descriptor for property pino is non-configurable and non-writable"
+        ),
+        true
+      );
+    }
+  });
 
-  const logger = Pino({ level: "info" });
-  logger.info("Hello");
-  console.log(stubbedPino);
+  test("Using a wrapper it should be easily possibile", () => {
+    const stubbedMethod = stub(PinoWrapped, "default");
+    PinoWrapped.default();
+    equal(stubbedMethod.calledOnce, true);
+  });
 });
